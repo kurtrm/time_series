8c60d42b75f532b6b156443ce1f845d157c5f56a
Kurt Maurer <kurtrm@gmail.com>
2018-09-18 10:27:24 -0700

    Need to work on other sql proficiencies.

--- psql_rolling_mean.sql
+++ psql_rolling_mean.sql
@@ -1,4 +1,5 @@
 DROP FUNCTION rolling_mean();
+DROP FUNCTIOn rolling_sum();
 
 CREATE OR REPLACE FUNCTION rolling_mean()
  RETURNS TABLE (
@@ -9,7 +10,23 @@ AS $$
 BEGIN
   RETURN QUERY SELECT
     date_trunc('hour', Date) as hour,
-    avg(ambient_temp)
+    AVG(ambient_temp)
+    OVER(order by date_trunc('hour', Date))
+  FROM
+    time_series;
+END;
+$$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION rolling_sum()
+ RETURNS TABLE (
+ eq_time TIMESTAMP,
+ eq_mean BIGINT
+)
+AS $$
+BEGIN
+  RETURN QUERY SELECT
+    date_trunc('hour', Date) as hour,
+    SUM(packing_leakage_rate)
     OVER(order by date_trunc('hour', Date))
   FROM
     time_series;
